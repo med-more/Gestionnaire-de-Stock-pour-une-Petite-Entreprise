e7685b2ee690efd275230f0815208ace6eaeb213
Mohammed Baba <mbfmz20@gmail.com>
2024-12-19 11:23:25 +0100

    ajouter la constructor et fonction pour charger les données

--- inventory.js
+++ inventory.js
@@ -2,5 +2,17 @@ const fs = require('fs');
 const Product = require('./product');
 
 class inventory{
+    constructor(filePath){
+        this.filePath = filePath;
+        this.products = this.loadProducts();
+    }
 
+    loadProducts(){
+        try{
+            const data = fs.readFileSync(this.filePath);
+            return JSON.parse(data);
+        } catch (error){
+            return [];
+        }
+    }
 }
